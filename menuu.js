9e7d9495853a0a5b3362763b39a9ae599197ba5d
nirmalamaviti <161691137+nirmalamaviti@users.noreply.github.com>
2024-05-07 10:30:18 +0530

    Add files via upload

---
+++ menuu.js
@@ -0,0 +1,238 @@
+const main_menu= [
+    {
+    id:1,
+    title:"dosa",
+    category:"TIFFIN",
+    price:"Rs.60/-",
+    img:"https://images.unsplash.com/photo-1668236543090-82eba5ee5976?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZG9zYXxlbnwwfHwwfHx8MA%3D%3D",
+    about:"In its most simple form, dosa is crispy and crepe-like and is a very popular street food in India. Dosa is famous for its simple ingredients and savory, slightly bitter flavor. ."
+    },
+    
+    {
+    id:2,
+    title:"IDLY",
+    category:"TIFFIN",
+    price:"Rs.35/-",
+    img:"https://t3.ftcdn.net/jpg/01/61/13/66/360_F_161136674_NgVFcPtWfwLPY03NpJUrSiH9oDvma9Rn.jpg",
+    about:"is a type of savoury rice cake, originating from South India, popular as a breakfast food in Southern India and in Sri Lanka. The cakes are made by steaming a batter consisting of fermented black lentils (de-husked) and rice ."
+    },
+    {
+    id:3,
+    title:"PONGAL",
+    category:"TIFFIN",
+    price:"Rs.49/-",
+    img:"https://media.istockphoto.com/id/678434780/photo/ven-pongal-traditional-indian-food.jpg?s=612x612&w=0&k=20&c=jqPqhh1SCHYvb7CrCSkd37_q2FwY8Nn-y5axLUG3Wvw=",
+    about:"Ven pongal recipe made with moong lentils, rice, spices and curry leaves. It is a traditional South Indian comfort food at its best! ."
+    },
+    {
+    id:4,
+    title:"PONGANALU",
+    category:"TIFFIN",
+    price:"Rs.29/-",
+    img:"https://static.toiimg.com/thumb/53286335.cms?width=1200&height=900",
+    about:"a traditional snack item of Andhra Pradesh, Telangana and Karnataka. It is a fluffy and smooth food item. It is sometimes stuffed with onions, corn or coriander for taste. ."
+    },
+    {
+    id:5,
+    title:"TEA",
+    category:"DRINKS",
+    price:"Rs.15/-",
+    img:"https://indianfoodfreak.com/wp-content/uploads/2023/05/IMG_20230521_162825.jpg",
+    about:"tea, beverage produced by steeping in freshly boiled water the young leaves and leaf buds of the tea plant, Camellia sinensis. Two principal varieties are used, the small-leaved China plant (C. sinensis variety sinensis)."
+    },
+    {
+    id:6,
+    title:"COFFEE",
+    category:"DRINKS",
+    price:"Rs.20/-",
+    img:"https://t4.ftcdn.net/jpg/01/05/90/77/360_F_105907729_4RzHYsHJ2UFt5koUI19fc6VzyFPEjeXe.jpg",
+    about:"Coffee is a beverage brewed from the roasted and ground seeds of the tropical evergreen coffee plant. Coffee is one of the three most popular beverages in the world (alongside water and tea), and it is one of the most profitable international commodities.."
+    },
+    {
+    id:7,
+    title:"MASALA CHAI",
+    category:"DRINKS",
+    price:"Rs.35/-",
+    img:"https://mir-s3-cdn-cf.behance.net/projects/404/124d42167702679.Y3JvcCw1NzI3LDQ0ODAsNDk2LDA.jpg",
+    about:"Masala Chai is an Indian beverage made by brewing black tea with fragrant spices, sugar and milk. In Hindi the word “Masala” means “spices” and “Chai” means “Tea”. So Masala Chai also known as CHAI TEA by "
+    },
+    {
+    id:8,
+    title:"LASSI",
+    category:"DRINKS",
+    price:"Rs.60/-",
+    img:"https://i.ytimg.com/vi/PbDPAqrGgBM/maxresdefault.jpg",
+    about:"A nutty blend of Nestlé A+ Curd and nuts is a coolant on hot days. It is a refreshing and healthy beverage and one of the favourite Indian beverage recipes made with yoghurt suitable for all age groups. ."
+    },
+    
+    {
+    id:9,
+    title:"FULL MEALS",
+    category:"LUNCH",
+    price:"Rs.150/-",
+    img:"https://live.staticflickr.com/8184/8143492351_9a0a239384_c.jpg",
+    about:"Chickens are average-sized fowls, characterized by smaller heads, short beaks and wings, and a round body perched on featherless legs. Exact size varies greatly among breeds, as does color. ."
+    },
+    
+    {
+    id:10,
+    title:"FULL MEALS",
+    category:"LUNCH",
+    price:"Rs.149/-",
+    img:"https://traditionallymodernfood.com/wp-content/uploads/2021/08/south-indian-lunch-combo-scaled.jpeg",
+    about:"Lunch is a meal eaten around the middle of the day. It is commonly the second meal of the day, after breakfast, and varies in size by culture and region.."
+    },
+    
+    {
+    id:11,
+    title:"LEMON RICE",
+    category:"LUNCH",
+    price:"Rs.99/-",
+    img:"https://rakskitchen.net/wp-content/uploads/2012/12/lemon-rice-1-500x375.jpg",
+    about:"Lemon Rice is a staple in many South Indian homes. It is made by mixing precooked short grain rice with a tempering of mustard seeds, chana dal, green chilies, dried red chilies."
+    },
+    
+    {
+    id:12,
+    title:"NONVEG MEALS",
+    category:"LUNCH",
+    price:"Rs.199/-",
+    img:" https://img.freepik.com/premium-photo/special-chicken-thali-from-indian-cuisine-food-platter-consists-chicken-curry-lentils-jeera-rice-roti-onions-selective-focus_726363-594.jpg",
+    abou:" food which contains whole or part of any animal including birds, marine animals, eggs, or products of any animal origin as an ingredient, excluding milk or milk products "
+    },
+    
+    {
+    id:13,
+    title:"MIRCHI BAJJI",
+    category:"SNACKS",
+    price:"Rs.30/-",
+    img:"https://www.indianhealthyrecipes.com/wp-content/uploads/2022/11/mirchi-bajji-mirapakaya-bajji-recipe.jpg" ,
+    about:"Mirchi translates to chilies and Bajji to gram flour fritters. For the uninitiated, Mirchi Bajji are green chilli fritters where fresh chilies are dipped in gram flour batter and deep .."
+    },
+    {
+    id:14,
+    title:"BONDAM",
+    category:"SNACKS",
+    price:"Rs.40/-",
+    img:"https://www.indianhealthyrecipes.com/wp-content/uploads/2023/01/punugulu-recipe.jpg" ,
+    about:"Bonda is a deep-fried South Indian potato snack that has various sweet and savory versions in different regions. The most common is aloo bonda (potato bonda), and other region-specific ."
+    },
+    {
+    id:15,
+    title:"BHEL",
+    category:"SNACKS",
+    price:"Rs.30/-",
+    img:"https://vegecravings.com/wp-content/uploads/2018/06/Bhel-Puri-Recipe-Step-By-Step-Instructions-1024x973.jpg.webp" ,
+    about:"Bhel is the Hindi term for puffed rice and puri means a round-shaped white bread deep-fried in oil. This roadside snack has a very twisted tangy flavour which attracts a large crowd. "
+    },
+    {
+    id:16,
+    title:"SWEET",
+    category:"SNACKS",
+    price:"Rs.100/-",
+    img:"https://cdn.prabhanews.com/wp-content/uploads/2024/01/image-86.png" ,
+    about:"also known as Kajjikaya, this is a sweet filling that comprises of dry grated coconut with sugar and optionally some cardamom. It is deep fried and its crispy ... ."
+    },
+    {
+    id:17,
+    title:"FISH CURRY",
+    category:"DINNER",
+    price:"Rs.150/-",
+    img:"https://indiantastes.in/wp-content/uploads/2022/12/bengali-fish-curry-recipe-1024x1024.png" ,
+    about:"Malabar matthi curry, also known as fish curry, is an Indian and Goan dish. It consists of sardines semi-stewed in a Kerala-style curry with assorted vegetables, such as okra or onions. It is usually served with rice or tapioca.."
+    },
+    {
+    id:18,
+    title:"RAGI BALL",
+    category:"DINNER",
+    price:"Rs.349/-",
+    img:"https://bl-i.thgim.com/public/migration_catalog/article18362017.ece/alternates/FREE_1200/BLink_Mudde%20and%20mutton%20korma",
+    about:"Ragi Mudde are gluten-free traditional balls made with finger millet flour. Finger millet is known as ragi in South India, and mudde is a Kannada word for soft ball. Ragi mudde are nutritious, healthy and a wholesome."
+    },
+    {
+    id:19,
+    title:"BAGARA&CHICKEN",
+    category:"DINNER",
+    price:"Rs.199/-",
+    img:"https://b.zmtcdn.com/data/dish_photos/65c/e79350b39d7ff648e20c7987220f765c.jpg",
+    about:"Spices seasoned in Ghee and Basmati Rice cooked in this flavorful seasoning in the true Telangana style is simply irresistible. A sure shot combination with this kind of Bagara Rice is the Telugu style spicy chicken curry. ."
+    },
+
+    {
+    id:20,
+    title:"EGG CURRY",
+    category:"DINNER",
+    price:"Rs.129/-",
+    img:" https://www.whiskaffair.com/wp-content/uploads/2021/02/Punjabi-Egg-Curry-2-3.jpg",
+    about:"This comforting Indian dish of boiled eggs simmered in a spicy onion tomato gravy is super delicious, flavorsome and simple to make. This egg curry has all those tasty and rustic flavors of the dhaba food that's sure to impress everyon."
+    },
+]
+
+
+
+
+const itemCategory = document.querySelector(".items"); 
+const btnContainer = document.querySelector(".header");
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(main_menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<div class="mess">
+        <div class="food">
+            <img src="${item.img}" alt="${item.title}">
+        </div>
+        <div class="about">
+            <div class="item_name">${item.title}</div>
+            <div class="cost">${item.price}</div>
+        </div>
+        <div class="foodabout">
+            <p>${item.about}</p>
+        </div>
+    </div>`;
+  });
+
+  displayMenu = displayMenu.join(""); 
+  itemCategory.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  const categories = main_menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" data-id="${category}">
+          ${category.toUpperCase()}
+        </button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = main_menu.filter(function (menuItem) {
+        if (category === "all") {
+          return true; 
+        }
+        return menuItem.category === category;
+      });
+      displayMenuItems(menuCategory);
+    });
+  });
+}
+
